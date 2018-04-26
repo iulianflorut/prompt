f0f0c58fb16821cfee663a7587497f561a43f61b
Iulian Florut <Iulian.Florut@cyanconnode.com>
2018-04-26 12:08:45 +0300

    add arguments

--- src/prompt/Prompt.java
+++ src/prompt/Prompt.java
@@ -9,13 +9,22 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Prompt {
 
 	private static final String FILE = "file=";
+	
+	private static List<String> arguments; 
+	
+	private static final Pattern argPattern = Pattern.compile("%\\d");
+
 
 	public static void main(String[] args) throws Exception {
 
@@ -28,6 +37,8 @@ public class Prompt {
 
 			Optional<File> file = Stream.of(args).filter(p -> p.startsWith(Prompt.FILE))
 					.map(p -> p.substring(Prompt.FILE.length())).map(File::new).findFirst();
+			
+			arguments = Stream.of(args).filter(p -> !p.startsWith(Prompt.FILE)).collect(Collectors.toList());
 
 			if (file.isPresent()) {
 				try (InputStream is = new FileInputStream(file.get())) {
@@ -57,7 +68,18 @@ public class Prompt {
 
 				if (Optional.ofNullable(line).isPresent()) {
 
-					final String cmd = line.trim();
+					String cmd = line.trim();
+					
+					
+					Matcher m = argPattern.matcher(cmd);
+					
+					while (m.find()) {
+						int index = Integer.valueOf(m.group().substring(1));
+						
+						if (arguments.size() >= index) {
+							cmd = cmd.replace(m.group(), arguments.get(index-1));
+						}
+					}
 
 					if (cmd.isEmpty()) {
 						continue;
